07c549336a3b9c65634eda4c007a7e946e86f10d
hkan <hermann.kan.dev@gmail.com>
2023-02-19 13:43:16 -0500

    test for linting

---
+++ deploy/test.js
@@ -0,0 +1,17 @@
+const guessesmade = 0
+const name = 'name'
+const description = `${guessesmade} "dedendedn"`
+
+if (guessesmade) {
+  console.log('x')
+
+  if (1 + 1) {
+    console.log(2)
+  }
+
+  if (description) {
+    console.log('description')
+  }
+} else {
+  console.log('dededon')
+}
